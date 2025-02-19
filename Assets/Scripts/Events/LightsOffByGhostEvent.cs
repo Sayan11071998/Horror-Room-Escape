030c724690fded857c1eb6f1bad0b40b60eaaf0d
Sayan Nandi <sayannandi11071998@gmail.com>
2025-02-19 21:35:08 +0530

    Made one small correction

--- Assets/Scripts/Events/LightsOffByGhostEvent.cs
+++ Assets/Scripts/Events/LightsOffByGhostEvent.cs
@@ -11,7 +11,7 @@ public class LightsOffByGhostEvent : MonoBehaviour
         {
             EventService.Instance.OnLightsOffByGhostEvent.InvokeEvent();
             GameService.Instance.GetSoundView().PlaySoundEffects(soundType);
-            this.enabled = false;
+            GetComponent<Collider>().enabled = false;
         }
     }
 }
